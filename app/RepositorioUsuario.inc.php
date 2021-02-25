36020e3580aefea372a92f1e08f620978a87061a
Pablo Bernabeu <pabc1@gcloud.ua.es>
2021-02-25 23:31:49 +0100

    Séptimo commit.
    Añadiendo los archivos de BootStrap.

--- app/RepositorioUsuario.inc.php
+++ app/RepositorioUsuario.inc.php
@@ -1,23 +1,23 @@
 <?php
 
 class RepositorioUsuario {
-    
+
     public static function obtener_todos($conexion){
-        
+
         $usuarios = array();
-        
+
         if(isset($conexion)){
-            
+
             try{
-                
+
                 include_once 'Usuario.inc.php';
-                
+
                 $sql = "select * from usuarios";
-                
+
                 $sentencia = $conexion -> prepare($sql);
                 $sentencia -> execute();
                 $resultado = $sentencia -> fetchAll();
-                
+
                 if(count($resultado)){
                     foreach($resultado as $fila){
                         $usuarios[] = new Usuario($fila['id'], $fila['nombre'], $fila['email'], $fila['password'], $fila['nombre'], $fila['fecha_registro'], $fila['activo']);
@@ -26,13 +26,37 @@ class RepositorioUsuario {
                 else{
                     print "No hay usuarios";
                 }
-                
+
             } catch (PDOException $ex) {
                 print "Error" . $ex -> getMessage();
             }
-            
+
         }
         return $usuarios;
     }
-    
+
+    public static function obtenerNumeroUsuarios($conexion){
+
+      $total_usuarios = null;
+
+      if(isset($conexion)){
+        try{
+
+          $sql = "select count(*) as total from usuarios";
+
+          $sentencia = $conexion -> prepare($sql);
+          $sentencia -> execute();
+          $resultado = $sentencia -> fetch();
+
+          $total_usuarios = $resultado['total'];
+
+        }
+        catch(PDOException $ex){
+          print 'ERROR' . $ex -> getMessage();
+        }
+      }
+      return $total_usuarios;
+
+    }
+
 }
