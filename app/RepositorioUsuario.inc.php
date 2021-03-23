08e371cb089c19bb671ce3732f3ff62717fe140d
Pablo Bernabeu <pabc1@gcloud.ua.es>
2021-03-24 00:39:07 +0100

    Vigesimo-octavo commit.
    Añadiendo estilo a la página de entrada única.

--- app/RepositorioUsuario.inc.php
+++ app/RepositorioUsuario.inc.php
@@ -183,4 +183,34 @@ class RepositorioUsuario {
 
     }
 
+    public static function obtener_usuario_por_id($conexion, $id){
+      $usuario = null;
+
+      if(isset($conexion)){
+        try{
+
+          include_once "Usuario.inc.php";
+
+          $sql = "select * from usuarios where id = :id";
+
+          $sentencia = $conexion->prepare($sql);
+          $sentencia->bindParam(":id", $id, PDO::PARAM_STR);
+
+          $sentencia->execute();
+
+          $resultado = $sentencia->fetch();
+
+          if(!empty($resultado)){
+            $usuario = new Usuario($resultado["id"], $resultado["nombre"], $resultado["email"], $resultado["password"], $resultado["fecha_registro"], $resultado["activo"]);
+          }
+
+        }
+        catch(PDOException $ex){
+          print "Error: " . ex.getMessage();
+        }
+      }
+      return $usuario;
+
+    }
+
 }
--- entrada.php
+++ entrada.php
@@ -6,6 +6,7 @@ include_once "app/Redireccion.inc.php";
 
 include_once "app/Entrada.inc.php";
 include_once "app/RepositorioEntrada.inc.php";
+include_once "app/RepositorioUsuario.inc.php";
 
 Conexion::abrir_conexion();
 
@@ -17,6 +18,7 @@ else{
 }
 
 $entrada = RepositorioEntrada::obtener_entrada_por_url(Conexion::obtener_conexion(), $url_entrada);
+$id_entrada = $entrada->obtener_id();
 Conexion::cerrar_conexion();
 
 ?>
@@ -25,6 +27,7 @@ Conexion::cerrar_conexion();
 <html>
 
   <head>
+    <title>Entrada <?php echo $id_entrada ?></title>
     <?php
       include_once "plantillas/headDeclaration.inc.php";
     ?>
@@ -34,7 +37,52 @@ Conexion::cerrar_conexion();
       include_once "plantillas/navbar.inc.php";
     ?>
 
-    <?php
+
+    <div class="container">
+      <div class="row dejar-espacio-entrada">
+        <div class="col-md-12">
+          <div>
+            <div class="titulo-entrada-v2">
+              <h3>
+                <strong>
+                  <?php
+                    echo $entrada->obtener_titulo();
+                  ?>
+                </strong>
+              </h3>
+            </div>
+            <div class="entrada-usuario">
+              <?php
+                $autor = RepositorioUsuario::obtener_usuario_por_id(Conexion::obtener_conexion(), $entrada->obtener_autor_id());
+              ?>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+              </svg> <?php echo "  " . $autor->obtener_nombre() ?>
+
+            </div>
+
+            <div class="entrada-fecha">
+              <?php
+                echo $entrada->obtener_fecha();
+              ?>
+            </div>
+
+            <div class="justificar-texto cuerpo-entrada-v2">
+              <?php
+                echo nl2br($entrada->obtener_texto());  // para hcer los saltos de linea.
+              ?>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <?php // añadir estilo a las entradas.
+      /*
       echo "<br>";
       echo "<br>";
       echo "<br>";
@@ -42,7 +90,8 @@ Conexion::cerrar_conexion();
       echo "<br>";
       echo $entrada->obtener_titulo();
       echo "<br>";
-      echo $entrada->obtener_texto();
+      echo nl2br($entrada->obtener_texto());
+      */
     ?>
 
     <?php
