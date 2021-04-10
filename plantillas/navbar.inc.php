567c846cdf29b0e4d033fb1088c79aceb20ec686
Pablo Bernabeu <pabc1@gcloud.ua.es>
2021-04-10 12:45:45 +0200

    Trigesimo-octavo commit.
    Cambiando algun estilo en css.

--- plantillas/navbar.inc.php
+++ plantillas/navbar.inc.php
@@ -8,7 +8,7 @@
 ?>
 
 <header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light ">
     <div class="container-fluid">
 
       <?php
@@ -99,7 +99,7 @@
                   <li><a class="dropdown-item" href="#">Cambiar contraseña</a></li>
                   <li><a class="dropdown-item" href="#">Borrar cuenta</a></li>
                   <li>
-                    <a href="<?php echo RUTA_LOGOUT; ?>">
+                    <a class="dropdown-item" href="<?php echo RUTA_LOGOUT; ?>">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
                         <path d="M7.5 1v7h1V1h-1z"/>
                         <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
--- quitar-entrada-favorita.php
+++ quitar-entrada-favorita.php
@@ -21,6 +21,4 @@
     Redireccion::redirigir(SERVIDOR);
   }
 
-
-
 ?>
