8dd4ed2f95845ee3aaf08c91ff2ac444f4ca597d
Pablo Bernabeu <pabc1@gcloud.ua.es>
2021-04-14 23:32:51 +0200

    Cuadragésimo-tercer commit.
    Añadiendo el config del hosting.

---
+++ bbdd/newConfig.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+define('NOMBRE_SERVIDOR', 'localhost');
+define('NOMBRE_USUARIO', 'id16532830_pablobernabeu1');
+define('PASSWORD', 'fSr?_~PUx5ZHk|@P');
+define('NOMBRE_BD', 'id16532830_blog');
+
+//Rutas de la web
+
+define("SERVIDOR", "https://pablobernabeuofficialpage.000webhostapp.com");
+define("RUTA_REGISTRO", SERVIDOR."/signInPage.php");
+define("RUTA_REGISTRO_CORRECTO", SERVIDOR."/registro-correcto.php");
+define("RUTA_LOGIN", SERVIDOR."/logInPage.php");
+define("RUTA_LOGOUT", SERVIDOR . "/logout.php");
+define("RUTA_ENTRADA", SERVIDOR . "/entrada.php");
+define("RUTA_PERFIL", SERVIDOR . "/perfil.php");
+define("RUTA_GESTOR_ENTRADAS", SERVIDOR . "/app/gestor-entradas.inc.php");
+define("RUTA_GESTOR_COMENTARIOS", SERVIDOR . "/gestor-comentarios.inc.php");
+define("RUTA_GESTOR_FAVORITOS", SERVIDOR . "/gestor-favoritos.inc.php");
+define("RUTA_INSERTAR_FAVORITOS", "GuardarEntradaFavorito.php");
+define("RUTA_ENTRADAS_FAVORITAS", SERVIDOR."/entradas-favoritas.php");
+define("RUTA_QUITAR_FAVORITAS", "quitar-entrada-favorita.php");
+define("RUTA_NUEVA_ENTRADA", SERVIDOR . "/nueva-entrada.php");
+
+// Recursos
+
+define("RUTA_CSS", SERVIDOR . "/css/");
+define("RUTA_JS", SERVIDOR . "/js/");
